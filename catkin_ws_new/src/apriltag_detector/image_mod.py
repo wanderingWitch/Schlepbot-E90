0c445ba530a8a6a3da877c3358b914b4295c1179
Schleppy the schlepbot <schleppy@schlepbot.notarealemailaddress>
2017-04-19 15:20:59 -0400

    added or maybe modified image_mod.py

--- catkin_ws_new/src/apriltag_detector/image_mod.py
+++ catkin_ws_new/src/apriltag_detector/image_mod.py
@@ -8,26 +8,39 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Point
 
-FRAME_RATE = rospy.Duration(1.0/50)
+fps = 50
+FRAME_RATE = rospy.Duration(1.0/fps)
 
 class getImage(object):
 
-def __init__(self):
-    # Set up image capture
-    rospy.init_node('image_capture')
-    self.camera = cv2.VideoCapture(0)
-    self.image_pub = rospy.Publisher("camera_raw", Image, latch=True)
-    self.bridge = CvBridge()
-    rospy.Timer(FRAME_RATE, self.image_callback)
+    def __init__(self):
+        # Set up image capture
+        rospy.init_node('image_capture')
+        self.camera = cv2.VideoCapture(0)
+        self.image_pub = rospy.Publisher("camera_raw", Image, latch=True)
+        self.bridge = CvBridge()
+        rospy.Timer(FRAME_RATE, self.image_callback)
 
-def image_callback(self, timer_event=None):
-    success, data = self.camera.read()
-    if not success:
-        # print 'failed to read'
-        rospy.loginfo('failed to read camera')
-        return
+    def image_callback(self, timer_event=None):
+        success, data = self.camera.read()
+        if not success:
+            # print 'failed to read'
+            rospy.loginfo('failed to read camera')
+            return
+        try:
+            success, cv_image = self.camera.read()
+            if not success:
+                pass
+            else:
+                self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+        except CvBridgeError as e:
+            print(e)
+    def run(self):
+        rospy.spin()
+
+if __name__== '__main__':
     try:
-        self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-        print(e)
-def run(self):
+        imager = getImage()
+        imager.run()
+    except RospyInterruptException:
+        pass
