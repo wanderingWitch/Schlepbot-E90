38e84bbf76a9643042638c77535347a242e3f48c
Mollie <mwild1@swarthmore.edu>
2017-04-22 15:34:03 -0400

    launch?

--- launch.sh
+++ launch.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-source /opt/ros/kinetic/setup.sh
+source /opt/ros/kinetic/share/setup.sh
 export ROS_PACKAGE_PATH=~/project_files/Schlepbot-E90/catkin_ws_new/src:$ROS_PACKAGE_PATH
 screen -d -m -S roscore roscore
 echo launched roscore
-screen -d -m -S raw rosrun apriltag_detector image_mod.py
-echo launched raw camera
-screen -d -m -S detect rosrun apriltag_detector detector.py
-echo launched apriltag detector
+# screen -d -m -S raw rosrun apriltag_detector image_mod.py
+# echo launched raw camera
+#screen -d -m -S detect rosrun apriltag_detector detector.py
+screen -d -m -S camera roslaunch apriltag_detector camera_launch.launch
+echo launched camera things
 sleep 30;
 #screen -d -m -S rosrun line_follower control.py
 #screen -d -m -S rosrun line_follower writer.py
